e55b602dc327000f211e5f673f1c679ae7803345
Abhay Goud <abhayajay15@gmail.com>
2025-08-10 16:44:41 +0530

    HOSTING SETTINGS

--- back-end/index.js
+++ back-end/index.js
@@ -12,7 +12,7 @@ import userRouter from './Routes/user.js'
 dotenv.config(); 
 const app=express();
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: process.env.FRONTEND_URL||"http://localhost:5173", 
   credentials:true,
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
