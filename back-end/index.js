42fe9852baceac49fb8e6ac062699173743e299f
Ace360Degree-CC <customercare@a360pl.com>
2026-03-20 16:16:12 +0530

    test commit

--- back-end/index.js
+++ back-end/index.js
@@ -8,34 +8,34 @@ import extendedRouter from './Routes/generateExtended.js'
 import videoRouter from './Routes/youtube.js'
 import authRouter from './Routes/auth.js'
 import userRouter from './Routes/user.js'
-
-dotenv.config(); 
-const app=express();
+//test commit 
+dotenv.config();
+const app = express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL, 
-  credentials:true,
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI,{
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(()=>console.log('Database connected successfully'))
-.catch((err)=>console.log('error connecting database:',err))
+  .then(() => console.log('Database connected successfully'))
+  .catch((err) => console.log('error connecting database:', err))
 
-app.use('/api',coreRouter);
-app.use('/api',extendedRouter);
-app.use('/api',videoRouter);
-app.use('/api/auth',authRouter);
-app.use('/api/user',userRouter);
+app.use('/api', coreRouter);
+app.use('/api', extendedRouter);
+app.use('/api', videoRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port,()=>{
-    console.log(`server started on port ${port}`) 
+app.listen(port, () => {
+  console.log(`server started on port ${port}`)
 })
 
 export default app;
